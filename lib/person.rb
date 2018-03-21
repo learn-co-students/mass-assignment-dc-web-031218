ad4b2ea85b9f789b289d92d0b3ac7c038feb110f
SaturdayAM <jwl62@georgetown.edu>
2018-03-21 10:07:00 -0400

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,11 @@
 class Person
   #your code here
+  attr_accessor :name, :birthday, :eye_color, :height, :weight,
+  				:handed, :complexion, :t_shirt_size,
+  				:wrist_size, :glove_size, :pant_length,
+  				:pant_width, :hair_color
+
+  def initialize(attr_hash)
+  	attr_hash.each{|key, val| self.send(("#{key}="), val)}
+  end
 end
